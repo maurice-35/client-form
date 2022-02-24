16e69aea1ceedf6695fd3b67c4f6206f8a1bd99b
mauricekollewe <kollewem.35@gmail.com>
2022-02-24 02:18:05 +0000

    create user models and user index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import userSchema from './models/user.js'
 
 
 const app = express();
@@ -7,14 +8,30 @@ const port = 5000
 const dbURI = 'mongodb://localhost/test-finer-v'  // name of data base or path.
 
 
+// define the models
+const User = mongoose.model('User', + userSchema)
+
+// log request
+app.use((req, _res, next) => {
+	console.log(`🏮 Incoming request: METHOD: ${req.method}, URL: ${req.url}`)
+	next()
+})
+
+// Index Route
+app.get('/users', async (_req, res) => {
+	const users = await User.find()
+	console.log('USERS', users)
+	return res.status(200).json(users)
+})
+
 const startServer = async () => {
   try {
 		await
 		mongoose.connect('mongodb://localhost/test-finer-v');
     console.log('🚨 Database has connected successfully')
 		app.listen(port, () =>
-      console.log(`🚀 Express is up and running on port ${port}`)
-    )
+      console.log(`🚀 Express is up and running on port ${port}`))
+
   } catch (err) {
     console.log(err);
     console.log("Something went wrong 🤔")
---
+++ models/user.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+
+
+//User Schema
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true, unique: true, maxLength: 30 },
+	surname: { type: String, required: true, unique: true, maxLength: 30 },
+  email: { type: String, required: true, unique: true },
+  
+})
+
+//Comment Schema
+const userComment = new mongoose.Schema({
+  telephone: { type: Number, required: true, unique: true, maxLength: 15 },
+  gender: { type: String, required: true, unique: true, maxLength: 10 },
+  dataOfbirth: { type: Number, required: true, unique: true },
+  
+})
+
+//Final Comment Schema
+  const finalCommentSchema = new mongoose.Schema({
+  comment: { type: String, required: true, maxLength: 500 },
+	
+  
+})
+
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform(_doc, json) {
+    // delete json.password
+    return json
+  }
+})
+
+
+export default mongoose.model('User', userSchema)
