4d4b5dc72e0a61608f2b01954f9d82664effb94a
mauricekollewe <kollewem.35@gmail.com>
2022-02-24 01:07:40 +0000

    dd connected

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ import mongoose from 'mongoose'
 
 const app = express();
 const port = 5000
- 
+const dbURI = 'mongodb://localhost/test-finer-v'  // name of data base or path.
 
 
-
-    // event listener
-    app.listen(port, () =>
+const startServer = async () => {
+  try {
+		mongoose.connect('mongodb://localhost/test-finer-v');
+    console.log('🚨 Database has connected successfully')
+		app.listen(port, () =>
       console.log(`🚀 Express is up and running on port ${port}`)
     )
- 
+  } catch (err) {
+    console.log(err);
+    console.log("Something went wrong 🤔")
+  }
+}
+startServer()
