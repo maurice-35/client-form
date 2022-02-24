bb4325c75caa7ef98bb3a0c63366c77ae4589a54
mauricekollewe <kollewem.35@gmail.com>
2022-02-24 14:30:43 +0000

    create db

--- db/data/users.js
+++ db/data/users.js
@@ -7,8 +7,6 @@ export default [
     gender: 'male',
     dateOfBirth: '02-02-2000',
     comment: 'Nice one'
-
-
   },
   {
     firstName: 'john',
@@ -20,36 +18,30 @@ export default [
     comment: 'I love it'
   },
   {
-    "_id": "62174ffc62c570cf1013ddfc",
-    "firstName": "susie",
-    "surname": "fox",
-    "email": "susie@email.com",
-    "telephone": 423,
-    "gender": "female",
-    "dateOfBirth": "05-02-1995",
-    "comment": "Nice one",
-    "__v": 0
+    firstName: 'jane',
+		surname: 'cook',
+    email: 'john@email.com',
+    telephone: 146,
+    gender: 'male',
+    dateOfBirth: '02-02-2000',
+    comment: 'I love it'
   },
   {
-    "_id": "621750b862c570cf1013ddfe",
-    "firstName": "jane",
-    "surname": "adams",
-    "email": "jane@email.com",
-    "telephone": 45111171123,
-    "gender": "female",
-    "dateOfBirth": "05-02-1990",
-    "comment": "Good to know",
-    "__v": 0
+    firstName: 'susie',
+		surname: 'fox',
+    email: 'susie@email.com',
+    telephone: 423,
+    gender: 'male',
+    dateOfBirth: '05-02-1995',
+    comment: 'Nice one'
   },
   {
-    "_id": "62177648891c7cdbdcc153f4",
-    "firstName": "jack",
-    "surname": "reed",
-    "email": "jack@email.com",
-    "telephone": 15112171123,
-    "gender": "male",
-    "dateOfBirth": "05-02-1992",
-    "comment": "Good to know",
-    "__v": 0
+    firstName: 'jack',
+    surname: 'reed',
+    email: 'jack@email.com',
+    telephone: 15112171123,
+    gender: 'male',
+    dateOfBirth: '05-02-1992',
+    comment: 'Good to know',
   }
 ]
--- db/seeds.js
+++ db/seeds.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose'
 import { dbURI } from '../config/environment.js'
+import User from '../models/user.js'
+import userData from './data/users.js'
 
 const seedDatabase = async () => {
 	try {
@@ -11,6 +13,10 @@ const seedDatabase = async () => {
 // drop db 
 		await mongoose.connection.db.dropDatabase()
 		console.log('👍🏾 DB dropped')
+
+	// create users using userData
+	const users = await User.create(userData)
+	console.log(`🍃 DB seeded with ${users .length} users`)
 
 	// closing the connection
 	await mongoose.connection.close()
