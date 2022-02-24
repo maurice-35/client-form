c4e02c4d1b2c9a16a38201b481b926f8445fcc6e
Maurice Kollewe T <84001897+maurice-35@users.noreply.github.com>
2022-02-24 12:07:48 +0000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -13,29 +13,5 @@ const userSchema = new mongoose.Schema({
   
 })
 
-// //Comment Schema
-// const userComment = new mongoose.Schema({
-//   telephone: { type: Number, required: true, unique: true, maxLength: 15 },
-//   gender: { type: String, required: true, unique: true, maxLength: 10 },
-//   dataOfbirth: { type: Number, required: true, unique: true },
-  
-// })
-
-// //Final Comment Schema
-//   const finalCommentSchema = new mongoose.Schema({
-//   comment: { type: String, required: true, maxLength: 500 },
-// })
-
-// define the models
-// const User = mongoose.model('User', + userSchema)
-
-
-// userSchema.set('toJSON', {
-//   virtuals: true,
-//   transform(_doc, json) {
-//     return json
-//   }
-// })
-
 
 export default mongoose.model('User', userSchema)
